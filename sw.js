63bd585266946fc0e62af8981612335e83db066c
makash839 <makash839@gmail.com>
2026-03-02 18:13:54 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE="akash-study-v1";
+
+self.addEventListener("install",e=>{
+ e.waitUntil(
+  caches.open(CACHE)
+   .then(c=>c.addAll([
+     "/",
+     "/dashboard"
+   ]))
+ );
+});
+
+self.addEventListener("fetch",e=>{
+ e.respondWith(
+  caches.match(e.request)
+   .then(r=>r||fetch(e.request))
+ );
+});
